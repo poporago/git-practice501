cc0bd01222cc0ee75ea9a641db88ba04e6adc3d9
poporago <dideoeo555@gmail.com>
2023-05-01 12:51:19 +0900

    in order to test, launch new file

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 # Test environment
 def print_hello():
-	animals = ['dog','cat','hamster','tiger']
-	foods = ['icecream','latte','bibimbap']
-	names = ['John','Jane','Gil-dong','Dong-eun',]
+	animals = [
+		'dog',
+		'cat',
+		'hamster',
+		'tiger']
+	foods = [
+		'icecream',
+		'latte'
+		,'bibimbap']
+	names = [
+		'John',
+		'Jane',
+		'Gil-dong',
+		'Dong-eun',]
 	for f_name in names:
-		print(f'hello, {f_name}')	
+		print(f'ihello, {f_name}')	
 
 if __name__ =='__main__':
 	print_hello()
